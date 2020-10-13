254c2bd1450c24c963fe049dcaf2a2f9d4da0cc3
Rai-Fox <rai_fox@mail.ru>
2020-10-13 15:18:38 +0300

    bigint-opt fixed
    insert, erase, begin, end, push_back, swap fixed

--- bigint-optimized/my_vector.h
+++ bigint-optimized/my_vector.h
@@ -5,14 +5,14 @@
 #include <cstdint>
 #include <vector>
 
-struct my_vector {
-	using vector = std::vector<uint32_t>;
+class my_vector {
+public:
 	uint32_t reference_count;
-	vector data;
+	std::vector<uint32_t> data;
 
 	my_vector() : reference_count(1) {};
 	my_vector(my_vector const& v) : reference_count(1), data(v.data) {};
-	explicit my_vector(vector const& v) : reference_count(1), data(v) {};
+	explicit my_vector(std::vector<uint32_t> const& v) : reference_count(1), data(v) {};
 	my_vector(uint32_t* begin, uint32_t* end) : reference_count(1), data(begin, end) {};
 
 	~my_vector() = default;
